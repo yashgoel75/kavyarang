20639011fa3272b2cdd0327e5e87656bbefa4644
yashgoel <yash.goel8370@gmail.com>
2025-10-19 16:41:37 +0530

    Welcome to KavyaRang

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function dashboard() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        My name is Lakhan
+      </div>
+    </>
+  );
+}
