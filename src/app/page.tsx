0b0ce85efb3306fb3bdaeb244d76008fecfe31cc
yashgoel <yash.goel8370@gmail.com>
2025-10-20 22:21:20 +0530

    add google font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,47 @@ export default function Home() {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push("/dashboard");
-    }, 3000); // Redirect after 3 seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push("/dashboard");
+  //   }, 3000);
 
-    return () => clearTimeout(timer);
-  }, [router]);
+  //   return () => clearTimeout(timer);
+  // }, [router]);
 
   return (
     <main className="main-container bg-[#2E2B26] flex flex-col items-center justify-center min-h-screen p-4">
-      <span className="text-4xl text-[#D8C3A5]">Welcome to</span>
+      <span className="text-4xl text-[#D8C3A5] dm-serif-display-regular-italic">
+        <GradientText
+          colors={[
+            "#9a6f0bff",
+            "#bd9864ff",
+            "#dbb56aff",
+            "#7f7464ff",
+            "#e9e99dff",
+          ]}
+          animationSpeed={5}
+          showBorder={false}
+          className="welcome-text"
+        >
+          Welcome to
+        </GradientText>
+      </span>
       <GradientText
-        colors={["#F5F5DC", "#D8C3A5", "#CBB994", "#A39887", "#F5F5DC"]}
+        colors={[
+          "#9a6f0bff",
+          "#bd9864ff",
+          "#dbb56aff",
+          "#7f7464ff",
+          "#e9e99dff",
+        ]}
         animationSpeed={5}
         showBorder={false}
-        className="custom-class text-[100px]"
+        className="custom-class text-[65px] md:text-[100px]"
       >
         KavyaRang
       </GradientText>
 
-      {/* Loading animation below KavyaRang */}
       {loading && (
         <div className="loader mt-8">
           <div></div>
